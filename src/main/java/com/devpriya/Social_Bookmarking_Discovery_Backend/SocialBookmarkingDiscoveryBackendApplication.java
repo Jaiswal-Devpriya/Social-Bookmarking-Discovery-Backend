587b623441b000f3951290e64fa7f223af660130
Jaiswal-Devpriya <jdevpriya2001@gmail.com>
2026-06-10 12:36:18 -0700

    Initial Spring Boot setup with PostgreSQL and Redis

---
+++ src/main/java/com/devpriya/Social_Bookmarking_Discovery_Backend/SocialBookmarkingDiscoveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devpriya.Social_Bookmarking_Discovery_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBookmarkingDiscoveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBookmarkingDiscoveryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpriya/Social_Bookmarking_Discovery_Backend/SocialBookmarkingDiscoveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpriya.Social_Bookmarking_Discovery_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBookmarkingDiscoveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
